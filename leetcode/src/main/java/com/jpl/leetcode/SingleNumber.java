847c4338a2100ce81df224597860a32abc360987
PANLONG JIA <panlong.jia@gmail.com>
2015-11-05 08:58:53 -0500

    leetcode 163

---
+++ leetcode/src/main/java/com/jpl/leetcode/SingleNumber.java
@@ -0,0 +1,11 @@
+package com.jpl.leetcode;
+
+public class SingleNumber {
+	public int singleNumber(int[] nums) {
+		int result = 0;
+		for(int i = 0; i < nums.length; i++){
+			result ^= nums[i];
+		}
+		return result;
+	}
+}
